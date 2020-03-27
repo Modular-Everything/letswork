093cbe32f56b08afa2e65e4d3ce613502120e572
Chrish Dunne <hey@chrish.design>
2020-03-27 16:33:05 +0000

    [-] Removed sitemap for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sitemap`,
     `gatsby-plugin-postcss`,
     `gatsby-plugin-emotion`,
     {
