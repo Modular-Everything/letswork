b33e884800b54e6010f824a0ee791bf2356761de
Chrish Dunne <hey@chrish.design>
2020-03-27 16:49:18 +0000

    [-] Removed mentions of date

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,7 +49,6 @@ export const data = graphql`
             Firstname
             Lastname
             Type
-            Creation_Date(fromNow: true)
             Country {
               data {
                 Name
