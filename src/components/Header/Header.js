4d9196f88358d1e38527e431439b1e5c8909e64e
Chrish Dunne <hey@chrish.design>
2020-03-28 10:48:05 +0000

    [+] Added landing page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// import { css } from '@emotion/core';
+import { css } from '@emotion/core';
 import styled from '@emotion/styled/macro';
 import tw from 'twin.macro';
 
@@ -16,6 +16,17 @@ const Header = () => {
         <LogoWrapper>
           <Logo />
         </LogoWrapper>
+        <ComingSoon>
+          <h1>
+            Coming soon{' '}
+            <span role="img" aria-label="Shifty eyes">
+              👀
+            </span>
+          </h1>
+          <p>
+            We&lsquo;re working hard to get this ready, please check back soon.
+          </p>
+        </ComingSoon>
       </Container>
     </HeaderWrap>
   );
@@ -27,12 +38,24 @@ export default Header;
 
 const HeaderWrap = styled.header(
   tw`
-    bg-grass py-8 mb-16
+    bg-grass py-8 mb-16 h-screen
   `,
 );
 
 const LogoWrapper = styled.div(
   tw`
     text-2xl
+  `,
+);
+
+const ComingSoon = styled.div(
+  tw`
+    mt-4 text-olive
+  `,
+
+  css`
+    & h1 {
+      ${tw`font-bold`}
+    }
   `,
 );
