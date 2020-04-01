47bbdd3ea8df8963d5fc976aee5189ffa271a589
Chrish Dunne <hey@chrish.design>
2020-04-01 17:18:34 +0100

    [+] Added metadata

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Helmet from 'react-helmet';
+import { graphql, useStaticQuery } from 'gatsby';
 
 // import { css } from '@emotion/core';
 import styled from '@emotion/styled/macro';
@@ -10,8 +12,27 @@ import Logo from '../Logo';
 // ====================================
 
 const Header = () => {
+  const data = useStaticQuery(graphql`
+    query SiteMeta {
+      site {
+        buildTime(fromNow: true)
+        siteMetadata {
+          title
+          author
+          description
+        }
+      }
+    }
+  `);
+
   return (
     <HeaderWrap>
+      <Helmet>
+        <title>{data.site.siteMetadata.title}</title>
+        <meta name="author" content={data.site.siteMetadata.author} />
+        <meta name="description" content={data.site.siteMetadata.description} />
+      </Helmet>
+
       <Container>
         <LogoWrapper>
           <Logo />
