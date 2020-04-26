062d49a5542335c4d43c9e612aa655d42e0dd823
Chrish Dunne <hey@chrish.design>
2020-04-26 12:36:51 +0100

    [+] Added ability to switch steps

--- src/pages/gether/submit-job.js
+++ src/pages/gether/submit-job.js
@@ -11,13 +11,17 @@ const About = () => {
     client: { clientName: '', clientJobTitle: '', clientCompany: '' },
   });
 
-  const [showForm, setShowform] = useState(true);
+  // ==============
+
+  const [activeStep, setActiveStep] = useState(1);
+  // const [isComplete, setIsComplete] = useState(false);
+  // const [isSent, setIsSent] = useState(false);
+  const totalSteps = 3;
 
   // ==============
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setShowform(false);
 
     setJobSpec({
       client: {
@@ -33,49 +37,80 @@ const About = () => {
   return (
     <Layout>
       <Container>
+        <h2>
+          Active Step: {activeStep}/{totalSteps}
+        </h2>
+
         <Form handleSubmit={handleSubmit}>
-          <Label>
-            <span>My name is</span>
-            <Input
-              type="text"
-              name="clientName"
-              placeholder="Forename Surname"
-              required
-            />
-          </Label>
+          <div style={{ opacity: activeStep === 1 ? '1' : '0' }}>
+            <div>
+              <Label>
+                <span>My name is</span>
+                <Input
+                  type="text"
+                  name="clientName"
+                  placeholder="Forename Surname"
+                  required
+                />
+              </Label>
 
-          <Label>
-            <span>I am a</span>
-            <Input
-              type="text"
-              name="clientJobTitle"
-              placeholder="Job Title"
-              required
-            />
-          </Label>
+              <Label>
+                <span>I am a</span>
+                <Input
+                  type="text"
+                  name="clientJobTitle"
+                  placeholder="Job Title"
+                  required
+                />
+              </Label>
 
-          <Label>
-            <span>My company is</span>
-            <Input
-              type="text"
-              name="clientCompany"
-              placeholder="Company Name"
-              required
-            />
-          </Label>
+              <Label>
+                <span>My company is</span>
+                <Input
+                  type="text"
+                  name="clientCompany"
+                  placeholder="Company Name"
+                  required
+                />
+              </Label>
+            </div>
+          </div>
 
-          <button type="submit">Submit</button>
+          <div style={{ opacity: activeStep === 2 ? '1' : '0' }}>
+            <div>Another step</div>
+          </div>
+
+          <div style={{ opacity: activeStep === 3 ? '1' : '0' }}>
+            <div>
+              <button type="submit">Submit</button>
+            </div>
+          </div>
         </Form>
 
-        {!showForm && (
-          <h2>
-            Your name is <strong>{jobSpec.client.clientName}</strong>
-            <br />
-            you are a <strong>{jobSpec.client.clientJobTitle}</strong>
-            <br />
-            and your company is <strong>{jobSpec.client.clientCompany}</strong>
-          </h2>
-        )}
+        <hr style={{ margin: '48px 0' }} />
+
+        <div>
+          {activeStep > 1 && (
+            <button type="button" onClick={() => setActiveStep(activeStep - 1)}>
+              Previous Step
+            </button>
+          )}
+          {activeStep !== totalSteps && (
+            <button type="button" onClick={() => setActiveStep(activeStep + 1)}>
+              Next Step
+            </button>
+          )}
+        </div>
+
+        <hr style={{ margin: '48px 0' }} />
+
+        <h2>
+          Your name is <strong>{jobSpec.client.clientName}</strong>
+          <br />
+          you are a <strong>{jobSpec.client.clientJobTitle}</strong>
+          <br />
+          and your company is <strong>{jobSpec.client.clientCompany}</strong>
+        </h2>
       </Container>
     </Layout>
   );
